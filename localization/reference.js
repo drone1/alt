b341ec6f9b2ec139f78c48978da1be15a749daa2
Jonathan Lippincott <jonlippincott@gmail.com>
2025-04-14 17:22:05 +0200

    localization: localization files for ALT's CLI

---
+++ localization/reference.js
@@ -0,0 +1,21 @@
+export default {
+	'msg-nothing-to-do': `Nothing to do`,
+	'msg-finished-with-errors': `Finished with %%errorsEncountered%% error%%s%%`,
+
+	'msg-translating': 'Translating...',
+	'msg-translating-key': `Translating %%key%%`,
+	'msg-preparing-endpoint-config': `Preparing endpoint configuration...`,
+	'msg-hitting-provider-endpoint': `Hitting %%providerName%% endpoint%%attemptStr%%...`,
+
+	'msg-no-update-needed-for-key': `No update needed for %%key%%`,
+	'msg-rate-limited-sleeping': `Rate limited; sleeping for %%interval%%s...%%attemptStr%%`,
+	'msg-show-translation-result': `Translated %%key%%: "%%newValue%%"`,
+	'msg-processing-lang-and-key': `[%%progress%%%] Processing %%targetLang%% – %%key%%...`,
+
+	'translation-reason-forced': `Forced update`,
+	'translation-reason-outputFileDidNotExist': `Output file %%outputFile%% did not exist`,
+	'translation-reason-userMissingReferenceValueHash': `No reference hash found`,
+	'translation-reason-userModifiedReferenceValue': `User modified reference string`,
+	'translation-reason-missingOutputKey': `No existing translation found`,
+	'translation-reason-missingOutputValueHash': `No hash found in cache file`
+}
