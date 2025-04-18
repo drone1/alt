380d60830a03ad1953945853f14e1d35860730b0
Jonathan Lippincott <jonlippincott@gmail.com>
2025-04-18 12:37:12 +0200

    minor: adds 'Available Models:' to --list-models

--- src/commands/list-models.js
+++ src/commands/list-models.js
@@ -2,5 +2,6 @@ import { loadTranslationProvider } from '../lib/provider.js'
 
 export async function runListModels({ appState, options, log }) {
 	const { apiKey, api } = await loadTranslationProvider({ __dirname: appState.__dirname, providerName: options.provider, log })
+	log.I(`Available models:\n`)
 	return log.I(await api.listModels(apiKey))
 }
